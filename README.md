af5ab1ea7d81c4df7d0adf3f6c0d772dc9c46826
TKB Studios <69647028+tkbstudios@users.noreply.github.com>
2023-07-18 11:31:39 +0200

    Preparing for version 1.20.1-0.0.4 Only a fex bug fixes left (reloadconf command)

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ ResourcePackSelector is a Bukkit plugin that allows players to select from a lis
 - Permission-based access: Resource packs can be associated with specific permission nodes, allowing server administrators to control which players can access each resource pack.
 - Random resource pack selection: The `/randomrep` command selects a random resource pack from the available options for the player, provided they have the necessary permission.
 - Configuration flexibility: The plugin's configuration file allows easy customization of resource packs, their associated URLs, and other settings.
+- Reload configuration: The `/reloadrpsconf` command allows server administrators to dynamically reload the plugin's configuration file without restarting the server.
+- Cooldowns: Server administrators can configure a cooldown period between resource pack changes to control how frequently players can switch resource packs.
 
 ## Permissions
 
@@ -18,6 +20,7 @@ To use this plugin, the following permission nodes are available:
 - `resourcepackselector.pack.custom2`: Allows the player to select the "Custom2" resource pack.
 - `resourcepackselector.command.changerep`: Allows the player to use the `/changerep` command.
 - `resourcepackselector.command.randomrep`: Allows the player to use the `/randomrep` command.
+- `resourcepackselector.reloadconf`: Allows server administrators to use the `/reloadrpsconf` command to reload the plugin's configuration file.
 
 Make sure to assign the appropriate permissions to the respective resource packs and commands in your permission manager.
 
@@ -29,9 +32,12 @@ Make sure to assign the appropriate permissions to the respective resource packs
 - `/randomrep`: Selects a random resource pack from the available options for the player. Requires the player to have the necessary permission node for at least one of the available resource packs (`resourcepackselector.pack.<pack>`).
   - Permission: `resourcepackselector.command.randomrep`
 
+- `/reloadrpsconf`: Reloads the ResourcePackSelector configuration file dynamically without restarting the server. Requires the `resourcepackselector.reloadconf` permission.
+  - Permission: `resourcepackselector.reloadconf`
+
 ## Configuration
 
-The configuration file `ResourcePackSelector/config.yml` allows you to specify the resource packs and their associated URLs. You can also configure the plugin settings in this file.
+The configuration file `ResourcePackSelector/config.yml` allows you to specify the resource packs, their associated URLs, and various plugin settings.
 
 The structure of the configuration file is as follows:
 
@@ -49,11 +55,19 @@ resourcePacks:
 
 Settings:
   promptAtJoin: true
+
+cooldown:
+  enabled: true
+  duration: 300 # Cooldown duration in seconds
 ```
 
 - `resourcePacks`: This section contains the resource packs available for selection. Each resource pack should have a unique key (e.g., "Default", "Custom1", "Custom2"). For each resource pack, specify the URL where the resource pack can be downloaded and the permission node required to use that resource pack.
 
 - `Settings.promptAtJoin`: If set to `true`, players will be prompted to select a resource pack upon joining the server. If set to `false`, the prompt will be skipped, and players will use the previously selected resource pack or the default resource pack.
+
+- `cooldown`: This section allows server administrators to configure the cooldown settings for changing resource packs.
+  - `cooldown.enabled`: Set to `true` to enable the cooldown feature, or `false` to disable it.
+  - `cooldown.duration`: The duration of the cooldown in seconds.
 
 Ensure that you have the correct URLs and permission nodes for the resource packs in the configuration file.
 
@@ -61,9 +75,10 @@ Ensure that you have the correct URLs and permission nodes for the resource pack
 
 1. Place the ResourcePackSelector.jar file in the `plugins` directory of your Bukkit server.
 2. Start the server to generate the default configuration file.
-3. Edit the `ResourcePackSelector/config.yml` file to add your desired resource packs, their URLs, and permission nodes.
+3. Edit the `ResourcePackSelector/config.yml` file to add your desired resource packs, their URLs, and permission nodes. Customize other plugin settings as needed.
 4. Restart the server for the changes to take effect.
 5. Players with the appropriate permissions can use the `/changerep` command to open the resource pack selection GUI and choose a resource pack.
 6. Players with the appropriate permissions can use the `/randomrep` command to select a random resource pack from the available options.
+7. Server administrators with the `resourcepackselector.reloadconf` permission can use the `/reloadrpsconf` command to dynamically reload the plugin's configuration file.
 
 That's it! With ResourcePackSelector, you can provide players with a curated selection of resource packs to enhance their gameplay experience on your server.
--- src/main/java/com/tkbstudios/resourcepackselector/ResourcePackSelector.java
+++ src/main/java/com/tkbstudios/resourcepackselector/ResourcePackSelector.java
@@ -1,7 +1,7 @@
 package com.tkbstudios.resourcepackselector;
 
-import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
@@ -17,16 +17,19 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.PluginLogger;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ResourcePackSelector extends JavaPlugin implements Listener {
+    Logger pluginLogger = PluginLogger.getLogger("ResourcePackSelector");
     private FileConfiguration config;
+    private Map<UUID, Long> cooldowns = new HashMap<>();
 
     @Override
     public void onEnable() {
@@ -35,18 +38,52 @@ public class ResourcePackSelector extends JavaPlugin implements Listener {
         if (!configFile.exists()) {
             // If it doesn't exist, save the default configuration
             saveDefaultConfig();
-            getLogger().info("Config.yml not found. Creating default configuration file.");
+            pluginLogger.log(Level.WARNING, "Config.yml not found. Creating default configuration file.");
         }
 
         // Load the configuration
         config = getConfig();
 
         getServer().getPluginManager().registerEvents(this, this);
+        pluginLogger.log(Level.INFO, ChatColor.GREEN + "ResourcePackSelector successfully loaded!");
     }
 
     @Override
     public void onDisable() {
         System.out.println("Disabled ResourcePackSelector!");
+    }
+
+    private void reloadConfigFile() {
+        pluginLogger.log(Level.WARNING, "Reloading.. config!");
+        reloadConfig();
+        config = getConfig();
+        pluginLogger.log(Level.INFO, "Reloaded config!");
+    }
+
+    private boolean isCooldownEnabled() {
+        return config.getBoolean("cooldown.enabled", true);
+    }
+
+    private int getCooldownDuration() {
+        return config.getInt("cooldown.duration", 300);
+    }
+
+    private boolean hasCooldownExpired(UUID playerId) {
+        if (!cooldowns.containsKey(playerId)) {
+            return true;
+        }
+        long lastChangeTimestamp = cooldowns.get(playerId);
+        int cooldownDuration = getCooldownDuration();
+        long currentTime = System.currentTimeMillis() / 1000L;
+        return (currentTime - lastChangeTimestamp) >= cooldownDuration;
+    }
+
+    private void setCooldown(UUID playerId) {
+        cooldowns.put(playerId, System.currentTimeMillis() / 1000L);
+    }
+
+    private void clearCooldown(UUID playerId) {
+        cooldowns.remove(playerId);
     }
 
     @EventHandler
@@ -66,6 +103,7 @@ public class ResourcePackSelector extends JavaPlugin implements Listener {
     private void openResourcePackSelectionGUI(Player player) {
         List<String> resourcePacks = new ArrayList<>();
         ConfigurationSection packsSection = config.getConfigurationSection("resourcePacks");
+        assert packsSection != null;
         for (String pack : packsSection.getKeys(false)) {
             String permission = packsSection.getString(pack + ".permission");
             if (permission != null && player.hasPermission(permission)) {
@@ -101,13 +139,20 @@ public class ResourcePackSelector extends JavaPlugin implements Listener {
         if (!(event.getWhoClicked() instanceof Player)) return;
         Player player = (Player) event.getWhoClicked();
 
-        if (!event.getView().getTitle().equals(ChatColor.GOLD + "Resource Pack Selection")) return;
+        if (!event.getView().getOriginalTitle().equals(ChatColor.GOLD + "Resource Pack Selection")) return;
         event.setCancelled(true);
 
         ItemStack clickedItem = event.getCurrentItem();
         if (clickedItem == null || clickedItem.getType() == Material.AIR) return;
 
-        String resourcePackName = ChatColor.stripColor(clickedItem.getItemMeta().getDisplayName());
+        if (isCooldownEnabled() && !hasCooldownExpired(player.getUniqueId())) {
+            long cooldownDuration = getCooldownDuration();
+            long remainingCooldown = (cooldowns.get(player.getUniqueId()) + cooldownDuration) - (System.currentTimeMillis() / 1000L);
+            player.sendMessage(ChatColor.RED + "You are still on cooldown. Please wait " + remainingCooldown + " seconds.");
+            return;
+        }
+
+        String resourcePackName = ChatColor.GREEN + clickedItem.getItemMeta().getDisplayName();
         config.set("players." + player.getUniqueId(), resourcePackName);
         saveConfig();
         sendResourcePack(player, resourcePackName);
@@ -115,18 +160,25 @@ public class ResourcePackSelector extends JavaPlugin implements Listener {
         player.closeInventory(); // Close the GUI
 
         player.playSound(player.getLocation(), Sound.ITEM_ARMOR_EQUIP_CHAIN, 1.0f, 1.0f);
+
+        if (isCooldownEnabled()) {
+            setCooldown(player.getUniqueId());
+            Bukkit.getScheduler().runTaskLater(this, () -> clearCooldown(player.getUniqueId()), getCooldownDuration() * 20L);
+        }
     }
 
     private void sendResourcePack(Player player, String resourcePackName) {
         ConfigurationSection packsSection = config.getConfigurationSection("resourcePacks");
-        if (!packsSection.contains(resourcePackName)) {
+        assert packsSection != null;
+        String strippedName = ChatColor.stripColor(resourcePackName);
+        if (!packsSection.contains(strippedName)) {
             player.sendMessage(ChatColor.RED + "Failed to find the resource pack configuration for " + resourcePackName);
             return;
         }
 
-        String resourcePackUrl = packsSection.getString(resourcePackName + ".url");
+        String resourcePackUrl = packsSection.getString(strippedName + ".url");
         if (resourcePackUrl != null) {
-            player.sendMessage(ChatColor.YELLOW + "Sending " + resourcePackName + " resource pack!");
+            player.sendMessage(ChatColor.GOLD + "Sending " + resourcePackName + " resource pack!");
             player.setResourcePack(resourcePackUrl);
             player.sendMessage(ChatColor.GREEN + "Sent " + resourcePackName + " resource pack!");
         } else {
@@ -134,8 +186,9 @@ public class ResourcePackSelector extends JavaPlugin implements Listener {
         }
     }
 
+
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, Command command, @NotNull String label, String[] args) {
         if (!command.getName().equalsIgnoreCase("changerep") && !command.getName().equalsIgnoreCase("randomrep")) {
             return false;
         }
@@ -153,14 +206,26 @@ public class ResourcePackSelector extends JavaPlugin implements Listener {
             selectRandomResourcePack(player);
         }
 
+        if (command.getName().equalsIgnoreCase("reloadrpsconf")) {
+            if (sender.hasPermission("resourcepackselector.reloadconf")) {
+                sender.sendMessage(ChatColor.GOLD + "Reloading ResourcePackSelector config..");
+                reloadConfigFile();
+                sender.sendMessage(ChatColor.GREEN + "ResourcePackSelector config reloaded!");
+            } else {
+                sender.sendMessage(ChatColor.RED + "You don't have permission to use this command.");
+            }
+            return true;
+        }
+
         return true;
     }
 
     private void selectRandomResourcePack(Player player) {
         List<String> resourcePacks = new ArrayList<>();
         ConfigurationSection packsSection = config.getConfigurationSection("resourcePacks");
+        assert packsSection != null;
         for (String pack : packsSection.getKeys(false)) {
-            if (player.hasPermission(packsSection.getString(pack + ".permission"))) {
+            if (player.hasPermission(Objects.requireNonNull(packsSection.getString(pack + ".permission")))) {
                 resourcePacks.add(pack);
             }
         }
